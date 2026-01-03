7c073378b6f2254e2f7df8f642aaf1a097273d0c
Pushpendra Singh <pushpendrasingh0549@gmail.com>
2026-01-03 11:41:44 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,5 +1,5 @@
-def call() {
-    if (params.RUN_OWASP) {
+def call(boolean runOwasp = true) {
+    if (runOwasp) {
         dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
         dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
     } else {
